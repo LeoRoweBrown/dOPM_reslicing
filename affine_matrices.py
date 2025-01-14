9a877e8c749e3a9d5d37badb65eb2c18b7f383a6
Rowe-Brown <leorowebrown@gmail.com>
2025-01-14 14:24:57 +0000

    affines working for 1 channel in generate_bdv_testing

--- affine_matrices.py
+++ affine_matrices.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-def scale_matrix(sx=1, sy=1, sz=1):
+def scale_matrix(sx=1, sy=1, sz=1, square=True):
     """
-    Generate a 4x4 affine matrix for scaling transformation.
+    Generate a 4x4 or 3x4 affine matrix for scaling transformation.
     
     Parameters:
     sx (float): Scaling factor along the x-axis.
@@ -10,70 +10,83 @@ def scale_matrix(sx=1, sy=1, sz=1):
     sz (float): Scaling factor along the z-axis.
     
     Returns:
-    np.ndarray: 4x4 affine matrix for scaling transformation.
-    """
-    return np.array([
+    np.ndarray: 4x4 or 3x4 (if square=False) affine matrix for scaling transformation.
+    """ 
+    matrix = np.array([
         [sx, 0, 0, 0],
         [0, sy, 0, 0],
         [0, 0, sz, 0],
         [0, 0, 0, 1]
     ])
+    if not square:
+        matrix = np.delete(matrix, (3), axis=0)
 
-def skew_matrix(theta_s):
+    return matrix
+
+def skew_matrix(theta_s, square=True):
     """
-    Generate a 4x4 affine matrix for skew transformation in yz plane.
+    Generate a 4x4 or 3x4 affine matrix for skew transformation in yz plane.
     
     Parameters:
     sz (float): Resulting angle of skew transformation in yz plane.
     
     Returns:
-    np.ndarray: 4x4 affine matrix for skew transformation.
+    np.ndarray: 4x4 or 3x4 (if square=False) affine matrix for skew transformation.
     """
-    return np.array([
+    matrix = np.array([
         [1, 0, 0, 0],
         [0, 1, theta_s, 0],
         [0, 0, 1, 0],
         [0, 0, 0, 1]
     ])
+    if not square:
+        matrix = np.delete(matrix, (3), axis=0)
 
-def rotation_matrix(theta, axis='z'):
+    return matrix
+
+def rotation_matrix(theta, axis='z', square=True):
     """
-    Generate a 4x4 affine matrix for rotation transformation about the x,
+    Generate a 4x4 or 3x4 affine matrix for rotation transformation about the x,
     y or z-axis.
     
     Parameters:
     theta (float): Rotation angle in radians.
     
     Returns:
-    np.ndarray: 4x4 affine matrix for rotation transformation.
+    np.ndarray: 4x4 or 3x4 (if square=False) affine matrix for rotation transformation.
     """
     cos_theta = np.cos(theta)
     sin_theta = np.sin(theta)
     if axis == 'z':
-        return np.array([
+        matrix = np.array([
             [cos_theta, -sin_theta, 0, 0],
             [sin_theta, cos_theta, 0, 0],
             [0, 0, 1, 0],
             [0, 0, 0, 1]
         ])
     elif axis == 'y':
-        return np.array([
+        matrix = np.array([
             [cos_theta, 0, sin_theta, 0],
             [0, 1, 0, 0],
             [-sin_theta, 0, cos_theta, 0],
             [0, 0, 0, 1]
         ])
-    elif axis == 'x':
-        return np.array([
+    else: # axis == 'x'
+        matrix = np.array([
             [1, 0, 0, 0],
             [0, cos_theta, -sin_theta, 0],
             [0, sin_theta, cos_theta, 0],
             [0, 0, 0, 1]
         ])
+    if not square: 
+        matrix = np.delete(matrix, (3), axis=0)
 
-def translation_matrix(tx, ty, tz=0):
+        
+    return matrix
+
+def translation_matrix(tx, ty, tz=0, square=True):
     """
-    Generate a 4x4 affine matrix for translation transformation.
+    Generate a 4x4 or 3x4 affine matrix for translation transformation.
     
     Parameters:
     tx (float): Translation along the x-axis.
@@ -81,11 +94,15 @@ def translation_matrix(tx, ty, tz=0):
     tz (float): Translation along the z-axis.
     
     Returns:
-    np.ndarray: 4x4 affine matrix for translation transformation.
+    np.ndarray: 4x4 or 3x4 (if square=False) affine matrix for translation transformation.
     """
-    return np.array([
+    matrix = np.array([
         [1, 0, 0, tx],
         [0, 1, 0, ty],
         [0, 0, 1, tz],
         [0, 0, 0, 1]
     ])
+    if not square:
+        matrix = np.delete(matrix, (3), axis=0)
+    
+    return matrix
