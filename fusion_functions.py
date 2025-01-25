65ccd8ef2af738aa476ba76881e3e58878d58a18
LeoRoweBrown <leorowebrown@gmail.com>
2025-01-25 17:29:53 +0000

    created pyimagej fusion script

--- fusion_functions.py
+++ fusion_functions.py
@@ -1,7 +1,6 @@
 import h5py
 import numpy as np
 from tifffile import imwrite, imread, tiffcomment, TiffFile
-from ome_types import from_tiff
 from glob import glob
 import os
 import affine_matrices as affine
@@ -16,52 +15,84 @@ import scyjava
 
 def deskew_views_separately():
     """deskew views with pyimagej and bigstitcher"""
+    pass
 
-def get_imagej_instance(memory_gb=60):
-    scyjava.config.add_option('-Xmx{memory_gb}g')
-    ij = imagej.init('sc.fiji:fiji:2.14.0')
+def get_imagej_instance(memory_gb=60, 
+                        imagej_dir="A:/leo_imagej/fiji-win64/fiji-win64/Fiji.app", 
+                        download_imagej=False,
+                        imagej_mode='interactive',
+                        imagej_download_options = ['sc.fiji:fiji:2.14.0', 
+                                                   'net.preibisch:BigStitcher:2.2.1']):
+    scyjava.config.add_option(f"-Xmx{memory_gb}g")
+
+    if 'ij' not in globals():
+        if download_imagej:
+            print("Downloading ImageJ")
+            ij = imagej.init(imagej_download_options, mode=imagej_mode)
+        elif imagej_dir is not None:
+            print(f"Opening ImageJ from {imagej_dir}")
+            ij = imagej.init(imagej_dir, mode='interactive')
+        else:
+            raise FileNotFoundError("No imageJ dir given, use download ImageJ=True")
+        print("Created ImageJ instance", ij.getVersion()) 
+    else:
+        print(f"ImageJ instance already exists ({ij.getVersion()})")
     return ij
 
-def run_reslicing_view_macro():
-    macro = """
-    #@ String name
-    #@ int age
-    #@ String city
-    #@output Object greeting
-    greeting = "Hello " + name + ". You are " + age + " years old, and live in " + city + "."
-    """
-    args = {
-        'name': 'Chuckles',
-        'age': 13,
-        'city': 'Nowhere'
+def deskew_and_save_tiffs(bdv_dir, save_dir, pattern='pos_*', separate_dirs=False):
+    ij = get_imagej_instance()
+    bdv_dirs = glob.glob(os.path.join(bdv_dir, pattern))
+    
+    for bdv in bdv_dirs:
+        dataset_xml_path = os.path.join(bdv, "dataset.xml")
+        print(f"reslicing data in {dataset_xml_path}")
+        filename_addition = os.path.basename(dataset_xml_path)
+        save_path = save_dir
+        if separate_dirs:
+            save_path = os.path.join(save_dir, filename_addition)
+        os.makedirs(save_path, exist_ok=True)
+        run_image_fusion_macro(ij, dataset_xml_path, save_dir, filename_addition=filename_addition)
+        
+def run_image_fusion_macro(ij, in_dir, out_dir,
+                           filename_addition="",
+                           downscale=4,
+                           save_fused=False,
+                           fusion_type="[Avg, Blending]",
+                           view='both'):
+    # Define the parameters
+    if not save_fused:
+        produce = "[Each view]"
+    else:
+        produce = "[Each timepoint & channel]"
+
+    if view == 'both':
+        angles = "[All angles]"
+    elif str(view) == '0':
+        angles = "[angle 0]"
+    elif str(view) == '90':
+        angles = "[angle 90]"
+        
+    params = {
+        "select": in_dir,
+        "process_angle": angles,
+        "process_channel": "[All channels]",
+        "process_illumination": "[All illuminations]",
+        "process_tile": "[All tiles]",
+        "process_timepoint": "[All Timepoints]",
+        "bounding_box": "[Currently Selected Views]",
+        "downsampling": str(downscale),
+        "interpolation": "[Linear Interpolation]",
+        "fusion_type": fusion_type,
+        "pixel_type": "[16-bit unsigned integer]",
+        "interest_points_for_non_rigid": "[-= Disable Non-Rigid =-]",
+        "produce": produce,
+        "fused_image": "[Save as (compressed) TIFF stacks]",
+        "define_input": "[Auto-load from input data (values shown below)]",
+        "output_file_directory": out_dir,
+        "filename_addition": f"[{filename_addition}_]"
     }
-    result = ij.py.run_macro(macro, args)
-    print(result.getOutput('greeting'))
+        
+    # Run the Image Fusion command with the parameters
+    result = ij.py.run_plugin("Image Fusion", params)
 
-    """
-    run("Image Fusion", 
-    "browse=A:/leo/micromanager/bone_marrow_2nd/aml/data/deskewed_20250119-160952/pos_p0000/dataset.xml 
-    select=A:/leo/micromanager/bone_marrow_2nd/aml/data/deskewed_20250119-160952/pos_p0000//dataset.xml 
-    process_angle=[All angles] 
-    process_channel=[All channels] 
-    process_illumination=[All illuminations] 
-    process_tile=[All tiles] 
-    process_timepoint=[All Timepoints] 
-    bounding_box=[Currently Selected Views] 
-    downsampling=1 
-    interpolation=[Linear Interpolation] 
-    fusion_type=[Avg, Blending] 
-    pixel_type=[16-bit unsigned integer] 
-    interest_points_for_non_rigid=[-= Disable Non-Rigid =-] 
-    produce=[Each timepoint & channel] 
-    fused_image=[Display using ImageJ] 
-    define_input=[Auto-load from input data (values shown below)] 
-    display=[precomputed (fast, complete copy in memory before display)] 
-    min_intensity=0 
-    max_intensity=255");
-
-    run("Image Fusion", "select=A:/leo/micromanager/bone_marrow_2nd/aml/data/deskewed_20250122-174424/pos_p0000//dataset.xml process_angle=[All angles] process_channel=[All channels] process_illumination=[All illuminations] process_tile=[All tiles] process_timepoint=[All Timepoints] bounding_box=[Currently Selected Views] downsampling=4 interpolation=[Linear Interpolation] fusion_type=[Avg, Blending] pixel_type=[16-bit unsigned integer] interest_points_for_non_rigid=[-= Disable Non-Rigid =-] produce=[Each timepoint & channel] fused_image=[Save as (compressed) TIFF stacks] define_input=[Auto-load from input data (values shown below)] output_file_directory=A:/leo/micromanager/bone_marrow_2nd/aml/data/deskewed_20250122-174424/pos_p0000 filename_addition=[]");
-
-    produce=[Each view]
-
-    """
+ 
