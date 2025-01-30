2bbf5ce01e9f420aa637359ab1d46e024ee0bc57
LeoRoweBrown <leorowebrown@gmail.com>
2025-01-30 09:56:39 +0000

    added windows command to fusion_functions (untested)

--- fusion_functions.py
+++ fusion_functions.py
@@ -10,6 +10,7 @@ from npy2bdv import BdvWriter
 from napari import Viewer
 import lxml
 from datetime import datetime
+import subprocess
 
 def deskew_views_separately():
     """deskew views with pyimagej and bigstitcher"""
@@ -35,47 +36,31 @@ def deskew_bdv_and_save_tiffs(bdv_dir, save_dir, pattern='pos_*',
         os.makedirs(save_path, exist_ok=True)
         run_image_fusion_macro(ij, dataset_xml_path, save_dir, filename_addition=filename_addition)
         
-def run_image_fusion_macro(ij, in_dir, out_dir,
+def run_image_fusion_macro(in_dir, out_dir, imagej_dir,
                            filename_addition="",
                            downscale=4,
                            save_fused=False,
                            fusion_type="[Avg, Blending]",
                            view='both',
                            correct_voxel_dims=True):
-    # Define the parameters
-    if not save_fused:
-        produce = "[Each view]"
-    else:
-        produce = "[Each timepoint & channel]"
 
-    if view == 'both':
-        angles = "[All angles]"
-    else:
-        angles = f"[angle {str(view)}]"
+    xml_path = os.path.join(in_dir, "dataset.xml")
 
-    save_method = "[Save as (compressed) TIFF stacks]"
-        
-    params = {
-        "select": in_dir,
-        "process_angle": angles,
-        "process_channel": "[All channels]",
-        "process_illumination": "[All illuminations]",
-        "process_tile": "[All tiles]",
-        "process_timepoint": "[All Timepoints]",
-        "bounding_box": "[Currently Selected Views]",
-        "downsampling": str(downscale),
-        "interpolation": "[Linear Interpolation]",
-        "fusion_type": fusion_type,
-        "pixel_type": "[16-bit unsigned integer]",
-        "interest_points_for_non_rigid": "[-= Disable Non-Rigid =-]",
-        "produce": produce,
-        "fused_image": save_method,
-        "define_input": "[Auto-load from input data (values shown below)]",
-        "output_file_directory": out_dir,
-        "filename_addition": f"[{filename_addition}_]"
-    }
-    print("Running macro")
-    # Run the Image Fusion command with the parameters
-    result = ij.py.run_plugin("Image Fusion", params)
+    # Build (windows) command
+    # first build param list
+    params = "["+
+            f"xml_path={xml_path} " +
+            f"out_dir={out_dir} " +
+            f"downscale={downscale} " +
+            f"fusion_method={fusion_method} " +
+            f"view={view} " +
+            f"filename_addition={filename_addition}" +
+            "]"
 
+    imagej_exe = os.path.join(imagej_dir, "ImageJ-win64.exe")
+    macro_path = "imagej_macros/image_fusion.ijm"
+    command = imagej_exe + " " + macro_path + params
+
+    print("Running command: " + command)
+    subprocess.run(command)
  
